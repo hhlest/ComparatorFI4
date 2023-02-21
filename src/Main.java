60e79f798855368ec030ce875b8b20ffd2bab9d0
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-21 19:47:23 +0200

    Variables names improvements

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,27 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(20, 6, 7, 12, 1, 8, 16, 19, 10));
+        List<Integer> list = new ArrayList<>(List.of(1, 64, 7, 70, 16, 19, 10));
         System.out.println(list);
 
-        Comparator<Integer> comp1 = (a,b) -> convert(a) - convert(b);
-        if (!isPrimeNumbersInList(list)) {
-            System.out.println(Collections.max(list, comp1));
-        } else {
-            System.out.println(Collections.min(list));
-        }
+        Comparator<Integer> comp1 = (a, b) -> convertIfNumberIsOdd(a) - convertIfNumberIsOdd(b);
+        if (isPrimeNumbersInList(list)) System.out.println(Collections.max(list, comp1));
+        if (!isPrimeNumbersInList(list)) System.out.println(Collections.min(list));
     }
 
-    public static Integer convert(Integer n) {
-        for (int i = 2; i < n; i++) {
-            if((n % i) == 0) {
-                return 0;
-            }
+    public static Integer convertIfNumberIsOdd(Integer n) {
+        if ((n % 2) == 0) {
+            return 0;
         }
         return n;
     }
 
     public static boolean isPrimeNumbersInList(List<Integer> l) {
         for (int i = 0; i < l.size(); i++) {
-            if((l.get(i) % 2) == 0) {
-                return false;
+            if ((l.get(i) % 2) != 0) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
