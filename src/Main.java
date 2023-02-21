57c66c34af1edc8a789190ccb782ed281bd97a7a
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-21 20:03:25 +0200

    Function<Integer, Integer> added

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,16 @@ public class Main {
         List<Integer> list = new ArrayList<>(List.of(1, 64, 7, 70, 16, 19, 10));
         System.out.println(list);
 
-        Comparator<Integer> comp1 = (a, b) -> convertIfNumberIsOdd(a) - convertIfNumberIsOdd(b);
+        Function<Integer, Integer> fun = a -> {
+            if ((a % 2) == 0) {
+                return 0;
+            }
+            return a;
+        };
+
+        Comparator<Integer> comp1 = (a, b) -> fun.apply(a) - fun.apply(b);
         if (isPrimeNumbersInList(list)) System.out.println(Collections.max(list, comp1));
         if (!isPrimeNumbersInList(list)) System.out.println(Collections.min(list));
-    }
-
-    public static Integer convertIfNumberIsOdd(Integer n) {
-        if ((n % 2) == 0) {
-            return 0;
-        }
-        return n;
     }
 
     public static boolean isPrimeNumbersInList(List<Integer> l) {
