d941dc876b4a62982bfdc124ae7d337b43e46702
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-21 18:36:07 +0200

    Logic improvements

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,32 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(20, 6, 4, 12, 1, 8, 16, 4, 10));
+        List<Integer> list = new ArrayList<>(List.of(20, 6, 7, 12, 1, 8, 16, 19, 10));
         System.out.println(list);
 
-        Comparator<Integer> comp1 = (a,b) -> primeCheck(a) - primeCheck(b);
-        System.out.println(Collections.max(list, comp1));
+        Comparator<Integer> comp1 = (a,b) -> convert(a) - convert(b);
+        if (!isPrimeNumbersInList(list)) {
+            System.out.println(Collections.max(list, comp1));
+        } else {
+            System.out.println(Collections.min(list));
+        }
     }
 
-    public static Integer primeCheck(Integer n) {
+    public static Integer convert(Integer n) {
         for (int i = 2; i < n; i++) {
             if((n % i) == 0) {
                 return 0;
             }
         }
         return n;
+    }
+
+    public static boolean isPrimeNumbersInList(List<Integer> l) {
+        for (int i = 0; i < l.size(); i++) {
+            if((l.get(i) % 2) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
